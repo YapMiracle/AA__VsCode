7d9d5f2fafeb4fd6cb0730b0065362a8ae6d5be7
YapMracle <2049829115@qq.com>
2022-03-24 14:47:42 +0800

    master

---
+++ Test/Test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+vector<int> D(505, 0);
+vector<int> pre(505, 0);
+vector<int> temp(505, 0);
+int main()
+{
+    for(int i=0; i<5; i++)
+        cout<<pre[i]<<" ";
+}
