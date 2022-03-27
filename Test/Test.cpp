51e5045ec5eb29048675c15dc9e5d929c87db477
YapMracle <2049829115@qq.com>
2022-03-27 17:48:40 +0800

    string类

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,16 +1,60 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstdio>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> D(505, 0);
-vector<int> pre(505, 0);
-vector<int> temp(505, 0);
+
+class String
+{
+    public:
+        String(const char* data = 0);
+        String(const String& str);
+        String & operator=(const String & str);
+        ~String();
+        char * get_c_str() const{return m_data;}
+    private:
+        char * m_data;
+};
+inline
+String::String(const char * data)
+{
+    if(data)
+    {
+        m_data=new char[strlen(data)+1];
+        strcpy(m_data, data);
+    }
+    else
+    {
+        m_data = new char[1];
+        *m_data = '\0';
+    }
+}
+inline
+String::String(const String &str)
+{
+    this->m_data= new char[strlen(str.m_data)+1];
+    strcpy(this->m_data, str.m_data);
+}
+inline
+String::~String()
+{
+    if(m_data)
+        delete[] m_data;
+}
+inline
+String& String::operator=(const String & str)
+{
+    if(this == &str)
+        return *this;
+    delete[] m_data;
+    m_data = new char[strlen(str.m_data)+1];
+    strcpy(m_data, str.m_data);
+    return *this;
+}
 int main()
 {
-    for(int i=0; i<5; i++)
-        cout<<pre[i]<<" ";
-    string s;
+    String s("123343");
+    String s1("123");
+    cout<<s.get_c_str()<<endl;
+    cout<<s1.get_c_str()<<endl;
+    s1=s;
+    cout<<s1.get_c_str()<<endl;
     
 }
